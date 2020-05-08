39f867f3c725bae6d2b61065d9f83477811bd06c
Jeanette Boitumelo <jbrasepae@gmail.com>
2020-05-08 11:28:37 +0200

    corrected project

--- animal.js
+++ animal.js
@@ -7,51 +7,55 @@ class Animal{
         console.log(this.name + ' eats')
     }
     sounds(){
-        return('makes noise')
+        return('sound')
     }
 }
 class Dog extends Animal{
     constructor(name){
         super(name)
     }
+
     sounds(){
         console.log('Dog barks')
     }
 }
-// var dog = new Dog('Rax')
-// dog.eat()
-// dog.sounds()
+
+var dog = new Dog('Rax')
+dog.eat()
+dog.sounds()
 
 class Cat extends Animal{
     constructor(name){
         super(name)
     }
+
     sounds(){
         console.log('Cat meows')
     }
 }
-// var cat = new Cat('Stormy')
-// cat.eat()
-// cat.sounds()
 
-let adoptedAnimals = [];
+var cat = new Cat('Stormy')
+cat.eat()
+cat.sounds()
+
 
 class Home {
+    constructor(){
+        this.adoptedAnimals = []
+    }
+
     adoptPet(pet){
-        adoptedAnimals.push(pet);
+        this.adoptedAnimals.push(pet);
     }
 
     makeAllSounds (){
-        for(var i = 0; i < adoptedAnimals.length; i++){
-            adoptedAnimals[i].sounds();
+        for(var i = 0; i < this.adoptedAnimals.length; i++){
+            this.adoptedAnimals[i].sounds();
         }
-    //        adoptPet.forEach(function(pet) {
-    //            return pet.sounds()
-               
-    //        });
      }
            
 }
+
 var home = new Home();
 var dog1 = new Dog();
 var dog2 = new Dog();
